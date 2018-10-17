cdbf755d97bbe07c05b814c2719f26569b4af5ed
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-17 09:44:10 -0400

    Configured Knexfile.js
    #Comment
    
    Added configiration including altering the filename

--- server.js
+++ server.js
@@ -2,3 +2,28 @@ const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+
+//Instatiates a server(app) object
+const app = express();
+
+//Third party middleware
+//express.json returns json objects of the response
+//All global middlewares that will be used across enpoints must also be plugged into the app
+//cors and helmet middlewares are not used
+app.use(express.json(), logger('combined'), cors(), helmet());
+
+//Root Request/Route Handler
+app.get('/', (req, res) => {
+    res.send('Test for root route')
+});
+
+
+
+
+
+
+
+const port = 9000;
+server.listen(port, function() {
+  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+});
