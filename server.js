00401e6876f9dfb0076a22fc6001aca6f9158ac7
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-17 13:33:08 -0400

    Added Create Enpoint
    #Comment
    
    Added endoint handler that persists data in to the data base

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 
 //Instatiates a server(app) object
 const app = express();
@@ -14,16 +18,29 @@ app.use(express.json(), logger('combined'), cors(), helmet());
 
 //Root Request/Route Handler
 app.get('/', (req, res) => {
-    res.send('Test for root route')
+    res.send('Test for root endpoint!')
 });
 
 
+//Create courses
+app.post('/api/courses', (req, res) => {
+    //Grab data from body
+    const course = req.body;
+    console.log(req.body);
+    //Save data from body
+    db.insert(course)
+        .into('courses')
+            .then(ids => {
+                req.statusCode(201).json(ids);
+            })
+                .catch(err => {
+
+                res.status(500).json(err);
+            });
+
+});
 
 
-
-
-
+//Port & Port Listner
 const port = 9000;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
-});
+app.listen(port, () => console.log(`\n Listening on on port ${port}`));
