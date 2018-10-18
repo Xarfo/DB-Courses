0b3892ac7c0d41df942f9c10a37310e992aca536
Abdulfatah Ahmad <abdulfatah.a.ahmad@gmx.com>
2018-10-17 22:36:24 -0400

    Added Update Endpoint
    #Comment
    
    This endpoint updates the name in the body of the request and returns count of the updated objects

--- server.js
+++ server.js
@@ -21,6 +21,34 @@ app.get('/', (req, res) => {
     res.send('Test for root endpoint!')
 });
 
+//Get courses
+app.get('/api/courses', (req, res) => {
+    //Get data
+    db('courses')
+            .then(courses => {
+                res.status(200).json(courses);
+            })
+                .catch(err => {
+                    res.status(500).json(err);
+            });
+
+});
+
+//Get courses by id
+app.get('/api/courses/:id', (req, res) => {
+    //Save data by id
+    const {id} = req.params;
+    console.log(id);
+    db('courses')
+        .where({id})
+            .then(course => {
+                res.status(200).json(course);
+            })
+                .catch(err => {
+                    res.status(500).json(err);
+            });
+
+});
 
 //Create courses
 app.post('/api/courses', (req, res) => {
@@ -31,16 +59,35 @@ app.post('/api/courses', (req, res) => {
     db.insert(course)
         .into('courses')
             .then(ids => {
-                req.statusCode(201).json(ids);
+                res.status(201).json(ids);
             })
                 .catch(err => {
+                    res.status(500).json(err);
+            });
 
-                res.status(500).json(err);
+});
+
+//Update courses
+app.put('/api/courses/:id', (req, res) => {
+    //Grab data from body
+    const {id} = req.params;
+    const {name} = req.body;
+    const updatedCourse = {name};
+    console.log(updatedCourse);
+    //Update  data of the body
+    db('courses')
+        .where({id})
+            .update(updatedCourse)
+                .then(updatedCourse => {
+                    res.status(201).json(updatedCourse);
+            })
+                        .catch(err => {
+                            res.status(500).json(err);
             });
 
 });
 
 
 //Port & Port Listner
-const port = 9000;
+const port = 8000;
 app.listen(port, () => console.log(`\n Listening on on port ${port}`));
